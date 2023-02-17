12e58ae120d41e92a73e0e9045c132a23b6ab683
D-Jy <duan@d-jy.net>
2023-02-17 21:47:50 +0800

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,18 @@ export const name = 'bingchat'
 
 const logger = new Logger(name)
 
-export const usage = "[自建后端转发服务](https://github.com/D-Jy-lab/koishi-bingchat-server)，目前尚未存在手把手教程，有基本的 python 使用经验一般可以较为轻松的搭建。"
+export const usage = `
+## 插件说明
+
+本插件参考openchat插件编写，需要自行搭建后端才能运行。
+
+[自建后端转发服务](https://github.com/D-Jy-lab/koishi-bingchat-server)，目前尚未存在手把手教程，有基本的 python 使用经验一般可以较为轻松的搭建。
+
+因官方接口更新频繁导致经常失效所以单独分离出后端转发服务。
+`
 
 export interface Config {
-  alias: string[]
+  alias: string[],
   watingMsg?: boolean,
   endPoint: string
 }
@@ -16,7 +24,7 @@ export interface Config {
 export const Config: Schema<Config> = Schema.object({
   alias: Schema.array(String).default(['bing', 'edgechat']).description('触发命令;别名'),
   watingMsg: Schema.boolean().description('等待响应前是否提示。').default(false),
-  endPoint: Schema.string().description('服务器地址').default('http://127.0.0.1:8007/chat')
+  endPoint: Schema.string().description('服务器地址').default('http://127.0.0.1:8006/bing')
 })
 
 export function apply(ctx: Context, config: Config) {
@@ -38,7 +46,7 @@ export function apply(ctx: Context, config: Config) {
   }
 
   const cmd = ctx.command(`${name} <提问内容:text>`)
-    .alias(config.alias[0], config.alias[1], config.alias[2])
+    .alias(config.alias[0], config.alias[1])
     .action(async ({ session }, input) => {
       if (!input?.trim()) return session.execute(`help ${name}`)
       if (config.watingMsg) session.send(session.text('.wating'))
